6b0a8ea639d97436b8f2f6c48d32d26cb0b092bf
Floyd <email2ike@gmail.com>
2018-11-24 11:07:39 -0500

    bringup H4K3RWalls wallpaper app

--- Android.mk
+++ Android.mk
@@ -13,10 +13,11 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES += $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4
 
-LOCAL_PACKAGE_NAME := AEXPapers
+LOCAL_PACKAGE_NAME := H4K3RWalls
 
 include $(BUILD_PACKAGE)
--- src/org/h4k3rom/h4k3rwalls/SolidColor.java
+++ src/org/h4k3rom/h4k3rwalls/SolidColor.java
@@ -7,7 +7,7 @@
  * or at https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-package org.aospextended.aexpapers;
+package org.h4k3rom.h4k3rwalls;
 
 import android.app.Activity;
 import android.app.AlertDialog;
